438b43790fed36483e3b9720c31b7e8040995826
Luca Clementi <luca.clementi@gmail.com>
2014-11-21 17:54:47 -0800

    fix bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,9 @@
 . $ROLLSROOT/etc/bootstrap-functions.sh
 
 
-ocaml-runtime
-gdbm-devel
-ocaml
+yum install gdbm-devel || exit 1
+
+mkdir -p RPMS/x86_64
+yumdownloader --resolve --destdir RPMS/x86_64/ --enablerepo epel ocaml ocaml-runtime || exit 1
+rpm -Uvh --force RPMS/x86_64/ocaml*.x86_64.rpm
+
